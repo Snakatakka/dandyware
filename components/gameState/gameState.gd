6426c6937900c7796ca84dff09bc14c354641107
Snakatakka <etwork563@gmail.com>
2026-04-24 19:18:40 -0400

    finished microgame class

--- components/gameState/gameState.gd
+++ components/gameState/gameState.gd
@@ -3,8 +3,12 @@ extends Node
 
 # this is essentially just a holder for all of the variables that have to stay consistent throughout a session
 
-static var stageNumber: int = 0
+static var stageNumber : int = 0
+static var heartNumber : int = 4
+static var gameSpeed : float = 1.0
 
 # resets all of the variables
 static func _reset():
 	stageNumber = 0
+	heartNumber = 4
+	gameSpeed = 1.0
--- components/microgames/microgame.gd
+++ components/microgames/microgame.gd
@@ -1,17 +1,21 @@
+@tool
+
+# note to future self, everytime you create a new microgame, copy over the timerr; the promptLabel; and the camera
+# if you don't do that, everything will break and you'll be very sad :(
 class_name Microgame
 extends Node2D
 
 #information about the microgame
-@export var microgameName: String
-@export var microgameDescription: String
-@export var microgamePrompt: String
-@export var microgameTimeLimit: int = 10
+@export var microgameName : String
+@export var microgameDescription : String
+@export var microgamePrompt : String
+@export var microgameTimeLimit : int = 10
 
-@onready var timer: Timer = $timer
-@onready var promptLabel: RichTextLabel = $promptLabel
+@onready var timer : Timer = $timer
+@onready var promptLabel : RichTextLabel = $promptLabel
 
-var isMicrogameWinConditionMet: bool
-var isMicrogameBossStage: bool
+var isMicrogameWinConditionMet : bool
+var isMicrogameBossStage : bool
 
 func _ready() -> void:
 	timer.wait_time = microgameTimeLimit
@@ -19,11 +23,17 @@ func _ready() -> void:
 
 # called whenvever the microgame is won
 func _win() -> void:
-	pass
+	if GameState.stageNumber % 20 == 0 and GameState.stageNumber < 4:
+		GameState.heartNumber += 1
+	
+	GameState.stageNumber += 1
 
 # called whenever the microgame is lost
 func _loss() -> void:
-	pass
+	GameState.heartNumber -= 1
+	
+	if GameState.stageNumber % 20 != 0:
+		GameState.stageNumber += 1
 
 func _onTimeout() -> void:
 	if isMicrogameWinConditionMet:
