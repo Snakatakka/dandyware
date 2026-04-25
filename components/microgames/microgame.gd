6a1c46302b89260108a3f2138e4236bd6bf34444
Snakatakka <etwork563@gmail.com>
2026-04-25 18:07:13 -0400

    improved microgame class

--- components/microgames/microgame.gd
+++ components/microgames/microgame.gd
@@ -1,9 +1,11 @@
 @tool
 
-# note to future self, everytime you create a new microgame, copy over the timerr; the promptLabel; and the camera
-# if you don't do that, everything will break and you'll be very sad :(
+## Basic class that holds all the logic for the microgames
 class_name Microgame
 extends Node2D
+
+# note to future self, everytime you create a new microgame, copy over the timerr; the promptLabel; and the camera
+# if you don't do that, everything will break and you'll be very sad :(
 
 #information about the microgame
 @export var microgameName : String
@@ -11,8 +13,9 @@ extends Node2D
 @export var microgamePrompt : String
 @export var microgameTimeLimit : int = 10
 
-@onready var timer : Timer = $timer
-@onready var promptLabel : RichTextLabel = $promptLabel
+@onready var timer: Timer = $microgameDependencies/timer
+@onready var promptLabel: RichTextLabel = $microgameDependencies/promptLabel
+@onready var textTimer: Timer = $microgameDependencies/promptLabel/textTimer
 
 var isMicrogameWinConditionMet : bool
 var isMicrogameBossStage : bool
@@ -40,3 +43,12 @@ func _onTimeout() -> void:
 		_win()
 	else:
 		_loss()
+
+func onTextTimerTimeout() -> void:
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	
+	tween.tween_property(promptLabel, "self_modulate:a", 0, 0.25)
+	tween.parallel().tween_property(promptLabel, "scale", Vector2(2, 2), 0.25)
+	tween.tween_callback(promptLabel.queue_free)
+	#print("timed out!")
+	#promptLabel.visible = false
